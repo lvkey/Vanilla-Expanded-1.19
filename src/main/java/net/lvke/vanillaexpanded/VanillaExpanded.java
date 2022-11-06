d72d6c65a3a22f30fe33673bdaefb7fd466ea286
lvke <luke.hillcoat.swift@gmail.com>
2022-11-06 21:02:44 +1000

    First Commit - Live on Github

--- src/main/java/net/lvke/vanillaexpanded/VanillaExpanded.java
+++ src/main/java/net/lvke/vanillaexpanded/VanillaExpanded.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class VanillaExpanded implements ModInitializer {
-	//Test Comment
+
 	public static final String MOD_ID = "vanillaexpanded";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
